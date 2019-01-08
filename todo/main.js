98ec51b3d42589ad642d569a20be1fc68f7fda14
ilko725 <ilko725@gmail.com>
2019-01-08 22:58:11 +0200

    add and delete task added

--- todo/main.js
+++ todo/main.js
@@ -1,30 +1,43 @@
 const app = {
-  tasks: [],
-  addTask: function(incomeTask) {
-    app.tasks.push(incomeTask);
-    app.renderTasks();
-  },
-  handleClick: function(e) {
-    e.preventDefault();
-    const val = document.querySelector('#enter').value;
-    app.addTask(val);
+    tasks: [],
+    addTask: function(incomeTask) {
+        if (app.tasks.indexOf(incomeTask) < 0) {
+            app.tasks.push(incomeTask);
+            app.renderTasks();
+        }
 
-    return false;
-  },
-  init: function() {
-    document.querySelector('#submit').addEventListener('click', app.handleClick); // TODO: check why not 'this.'
-  },
-  renderTasks: function() {
-    const list = document.querySelector('#tasks');
-    let tasks = '';
+    },
+    deleteTask: function(task) {
+        let indOf = app.tasks.indexOf(task)
+        if (indOf >= 0) {
+            app.tasks.splice(indOf, 1);
+            app.renderTasks();
+        }
+    },
+    handleClick: function(e) {
+        e.preventDefault();
+        const val = document.querySelector('#enter').value;
+        app.addTask(val);
 
-    for (let task of app.tasks) {
-      tasks += `<li class="task">${task}</li>`;
+        return false;
+    },
+    init: function() {
+        document.querySelector('#submit').addEventListener('click', app.handleClick); // TODO: check why not 'this.'
+    },
+    renderTasks: function() {
+        const list = document.querySelector('#tasks');
+        let tasks = '';
+
+        for (let task of app.tasks) {
+            tasks += `<li class="task">${task}</li>`;
+        }
+
+        list.innerHTML = tasks;
+        list.querySelector('li').addEventListener('click', function(arg) {
+            var task = arg.target.innerText;
+            app.deleteTask(task);
+        });
     }
-
-    list.innerHTML = tasks;
-  }
 }
 
 document.addEventListener("DOMContentLoaded", app.init);
-
