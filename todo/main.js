e58702582cf3da66d79c94829a6756e90ad789cb
Leo <eleleka@skilld.fr>
2019-01-14 22:58:21 +0200

    local storage part2

--- todo/main.js
+++ todo/main.js
@@ -1,55 +1,61 @@
 window.todoApp = {
-    app: {
-        tasks: [],
-        addTask: function(incomeTask) {
-            if (app.tasks.indexOf(incomeTask) < 0) {
-                app.tasks.push(incomeTask);
-                app.renderTasks();
-                window.todoApp.saveData(app.tasks)
-            }
-        },
-        deleteTask: function(task) {
-            let indOf = app.tasks.indexOf(task);
+  app: {
+    tasks: [],
+    addTask: function(incomeTask) {
+      if (app.tasks.indexOf(incomeTask) < 0) {
+        app.tasks.push(incomeTask);
+        app.renderTasks();
+        window.todoApp.saveData(app.tasks)
+      }
+    },
+    deleteTask: function(task) {
+      let indOf = app.tasks.indexOf(task);
 
-            if (indOf >= 0) {
-                app.tasks.splice(indOf, 1);
-                app.renderTasks();
-                window.todoApp.unsetData(task)
-            }
-        },
-        handleClick: function(e) {
-            e.preventDefault();
-            const val = document.querySelector('#enter').value;
+      if (indOf >= 0) {
+        app.tasks.splice(indOf, 1);
+        app.renderTasks();
+        window.todoApp.saveData(app.tasks)
+      }
+    },
+    handleClick: function(e) {
+      e.preventDefault();
+      const val = document.querySelector('#enter').value;
 
-            if (val.length) {
-                app.addTask(val);
-            }
+      if (val.length) {
+        app.addTask(val);
+      }
 
-            return false;
-        },
-        init: function() {
-            const localTasks = window.todoApp.fetchData()
-            if (typeof localTasks === 'object') {
-                app.tasks = localTasks;
-            }
-            document.querySelector('#submit').addEventListener('click', app.handleClick); // TODO: check why not 'this.'
-        },
-        renderTasks: function() {
-            const list = document.querySelector('#tasks');
-            let tasks = '';
+      return false;
+    },
+    init: function() {
+      const localTasks = window.todoApp.fetchData()
+      if (typeof localTasks === 'object' && localTasks.length) {
+        app.tasks = localTasks;
+        app.renderTasks();
+      }
+      document.querySelector('#submit').addEventListener('click', app.handleClick); // TODO: check why not 'this.'
+    },
+    renderTasks: function() {
+      const list = document.querySelector('#tasks');
+      let tasks = '';
 
-            for (let task of app.tasks) {
-                tasks += `<li class="task">${task}</li>`;
-            }
+      for (let task of app.tasks) {
+        tasks += `<li class="task">${task}</li>`;
+      }
 
-            list.innerHTML = tasks;
+      list.innerHTML = tasks;
 
-            list.querySelector('li').addEventListener('click', function(arg) {
-                var task = arg.target.innerText;
-                app.deleteTask(task);
-            });
-        }
+      list.querySelector('li').addEventListener('click', function(arg) {
+        var task = arg.target.innerText;
+        app.deleteTask(task);
+      });
+      app.updateCounter()
+    },
+    updateCounter: function() {
+      document.querySelector('#counter').innerText = app.tasks.length
     }
+  }
 }
+
 const app = window.todoApp.app;
 document.addEventListener("DOMContentLoaded", app.init);
