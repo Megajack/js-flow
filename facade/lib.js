d869d1e469e0345c8ea59c8b0da3cd0057de5df6
ilko725 <ilko725@gmail.com>
2019-01-26 22:27:48 +0200

    facade added

---
+++ facade/lib.js
@@ -0,0 +1,13 @@
+(function($){
+	window.lib = {
+		classCheck: function(btn, className){
+			console.log(this)
+			return $(btn).hasClass(className);
+		},
+		elemDelete: function () {
+		},
+		setHtml: function () {
+			// body...
+		}
+	}
+})(jQuery)
