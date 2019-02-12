270271b8879f689e52061c92c5d21e08df688ed2
Leo <eleleka@skilld.fr>
2019-02-12 22:45:48 +0200

    add progress bar

--- player/controller.js
+++ player/controller.js
@@ -1,8 +1,14 @@
 window.videoController = {
 	init: function (video) {
-		console.log(video)
+		// console.log(video)
 		this.video = video
 		console.dir(video)
+		this.video.ontimeupdate = this.timeUpdate.bind(this)
+	},
+	timeUpdate: function() {
+		// console.log(this.video.currentTime)
+		// return (this.video.currentTime * 100) / this.video.duration
+		window.playerUi.playbackPosition((this.video.currentTime * 100) / this.video.duration)
 	},
 	pauseVideo: function () {
 		this.video.pause()
--- player/ui.js
+++ player/ui.js
@@ -1,19 +1,23 @@
 window.playerUi = {
 	init: function(){
-		console.log('inited')
+		// console.log('inited')
 		this.playButton = document.querySelector('#play')
 		this.backwardButton = document.querySelector('#backward')
 		this.forwardButton = document.querySelector('#forward')
 		this.video = document.querySelector('video')
+		this.progress = document.querySelector('.slider-fill')
 		this.playButton.addEventListener('click', this.buttonClick.bind(this))
 		videoController.init(this.video)
 	},
 	buttonClick: function() {
-		console.log(this)
+		// console.log(this)
 		if (this.video.paused) {
 			videoController.playVideo()
 		} else {
 			videoController.pauseVideo()
 		}
+	},
+	playbackPosition: function(perc) {
+		this.progress.style.width = perc + '%'
 	}
 }
