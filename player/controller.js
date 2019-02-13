a85b7d3c23fa6c4aba24b7a8141c0f2258cedeb6
Leo <eleleka@skilld.fr>
2019-02-13 02:26:44 +0200

    add back/forward buttons behaviors

--- player/controller.js
+++ player/controller.js
@@ -1,19 +1,24 @@
 window.videoController = {
 	init: function (video) {
-		// console.log(video)
 		this.video = video
 		console.dir(video)
 		this.video.ontimeupdate = this.timeUpdate.bind(this)
 	},
 	timeUpdate: function() {
-		// console.log(this.video.currentTime)
-		// return (this.video.currentTime * 100) / this.video.duration
 		window.playerUi.playbackPosition((this.video.currentTime * 100) / this.video.duration)
 	},
-	pauseVideo: function () {
+	pauseVideo: function() {
 		this.video.pause()
 	},
-	playVideo: function () {
+	playVideo: function() {
 		this.video.play()
+	},
+	backPlay: function() {
+		window.playerUi.playbackPosition(this.video.currentTime - 10)
+		this.video.currentTime = this.video.currentTime - 10
+	},
+	forwardPlay: function() {
+		window.playerUi.playbackPosition(this.video.currentTime + 10)
+		this.video.currentTime = this.video.currentTime + 10
 	}
 }
--- player/ui.js
+++ player/ui.js
@@ -1,12 +1,14 @@
 window.playerUi = {
 	init: function(){
-		// console.log('inited')
 		this.playButton = document.querySelector('#play')
 		this.backwardButton = document.querySelector('#backward')
 		this.forwardButton = document.querySelector('#forward')
 		this.video = document.querySelector('video')
 		this.progress = document.querySelector('.slider-fill')
+
 		this.playButton.addEventListener('click', this.buttonClick.bind(this))
+		this.backwardButton.addEventListener('click', this.backClick.bind(this))
+		this.forwardButton.addEventListener('click', this.forwardClick.bind(this))
 		videoController.init(this.video)
 	},
 	buttonClick: function() {
@@ -16,6 +18,13 @@ window.playerUi = {
 		} else {
 			videoController.pauseVideo()
 		}
+	},
+	// TODO: make single method with Switch
+	backClick: function() {
+		window.videoController.backPlay()
+	},
+	forwardClick: function() {
+		window.videoController.forwardPlay()
 	},
 	playbackPosition: function(perc) {
 		this.progress.style.width = perc + '%'
